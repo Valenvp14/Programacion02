dc38f9b84b7aa4453abcd3c7b82d8081e98eccb3
Valenvp14 <93169177+Valenvp14@users.noreply.github.com>
2022-03-14 23:36:19 -0400

    Create modelo.py

---
+++ modelo.py
@@ -0,0 +1,9 @@
+vAxiomas = ["Entre tres puntos, solo pasa una línea recta", "Una proposición no puede ser verdadera y falsa al mismo tiempo", "Si dos números naturales tienen el mismo sucesor, esos dos números son el mismo número.",
+            "Todas las rectas tienen una cantidad infinita de puntos.", "Dos rectas paralelas nunca se tocan", "Dos líneas rectas nunca encierran algo."]
+
+
+class axiomaModel:
+    def obtenerAxioma(self, vNroEscogido):
+        vResult = vAxiomas[vNroEscogido]
+        return vResult
+
